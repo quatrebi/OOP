578631ebb9c9b18003345835df3d702de3da0c96
Dmitriy Khudnitskiy (VS) <dimon.h02@gmail.com>
2020-09-15 23:59:28 +0300

    Fixed tabs in 1-a task. DESIGN !

--- Lab-1/Application.cs
+++ Lab-1/Application.cs
@@ -15,26 +15,26 @@ namespace Lab_1
         {
             // 1 - a
             // - Value Types
-            bool boolVar = true;                     // System.Boolean;
-            char charVar = char.MaxValue;            // System.Char
+            bool boolVar             = true;                     // System.Boolean;
+            char charVar             = char.MaxValue;            // System.Char
 
-            sbyte sbyteVar = sbyte.MinValue;           // System.SByte (s8-bit)
-            byte byteVar = byte.MaxValue;            // Systen.Byte (u8-bit)
-            short shortVar = short.MinValue;           // System.Int16 (s16-bit)
-            ushort ushortVar = ushort.MaxValue;          // System.UInt16 (u16-bit)
-            int intVar = int.MinValue;             // System.Int32 (s32-bit)
-            uint uintVar = uint.MaxValue;            // System.UInt32 (u32-bit)
-            long longVar = long.MinValue;            // System.Int64 (s64-bit)
-            ulong ulongVar = ulong.MaxValue;           // System.UInt64 (u64-bit)
+            sbyte sbyteVar           = sbyte.MinValue;           // System.SByte (s8-bit)
+            byte byteVar             = byte.MaxValue;            // Systen.Byte (u8-bit)
+            short shortVar           = short.MinValue;           // System.Int16 (s16-bit)
+            ushort ushortVar         = ushort.MaxValue;          // System.UInt16 (u16-bit)
+            int intVar               = int.MinValue;             // System.Int32 (s32-bit)
+            uint uintVar             = uint.MaxValue;            // System.UInt32 (u32-bit)
+            long longVar             = long.MinValue;            // System.Int64 (s64-bit)
+            ulong ulongVar           = ulong.MaxValue;           // System.UInt64 (u64-bit)
 
-            float floatVar = float.MinValue;           // System.Single (4-bytes)
-            double doubleVar = double.MaxValue;          // System.Double (8-bytes)
-            decimal decimalVar = decimal.MinValue;         // System.Decimal (16-bytes)
+            float floatVar           = float.MinValue;           // System.Single (4-bytes)
+            double doubleVar         = double.MaxValue;          // System.Double (8-bytes)
+            decimal decimalVar       = decimal.MinValue;         // System.Decimal (16-bytes)
 
             // - References Types
-            object objectVar = new object();             // System.Object
-            string stringVar = new string(new char[1]);  // System.String
-            dynamic dynamicVar = new object();             // System.Object
+            object objectVar         = new object();             // System.Object
+            string stringVar         = new string(new char[1]);  // System.String
+            dynamic dynamicVar       = new object();             // System.Object
 
             // 1 - b
             // - Неявное преобразование (возможна потеря данных)
