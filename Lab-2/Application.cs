e919b9ed1af6fbe12b73bfe0ef48bb6905ff7d15
Dmitriy Khudnitskiy (VS) <dimon.h02@gmail.com>
2020-09-22 22:19:08 +0300

    Конструкторы, статики и прочее

--- Lab-2/Application.cs
+++ Lab-2/Application.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Dynamic;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lab_2
 {
     class Application
     {
+        public static Random rand;
         static void Main(string[] args)
         {
+            rand = new Random(DateTime.Now.Millisecond);
+
             Driver driver = new Driver();
             Bus bus = new Bus
             {
@@ -24,9 +29,10 @@ namespace Lab_2
         }
     }
 
-    public class Bus
+    public partial class Bus
     {
         // Fields
+        private readonly long _id;
         private Driver _driver;
         private string _busNumber;
         private short _wayNumber;
@@ -34,7 +40,14 @@ namespace Lab_2
         private DateTime _startDate;
         private long _mileage;
 
+        private static string[] m_availableBrands;
+        private const short m_maxSpeed = 90;
+
         // Properties
+        public long ID
+        {
+            get { return _id; }
+        }
         public Driver Driver
         {
             get { return _driver; }
@@ -55,13 +68,25 @@ namespace Lab_2
         public short WayNumber
         {
             get { return _wayNumber; }
-            set { _wayNumber = value; }
+            set
+            {
+                if (value > 0)
+                    _wayNumber = value;
+                else
+                    Console.Error.WriteLine("[Class.Bus.WayNumber] Incorrect way number");
+            }
         }
 
         public string Brand
         {
             get { return _brand; }
-            set { _brand = value; }
+            set
+            {
+                if (m_availableBrands.Contains(value))
+                    _brand = value;
+                else
+                    Console.Error.WriteLine("[Class.Bus.Brand] Unavailable brand.");
+            }
         }
 
         public DateTime StartDate
@@ -76,7 +101,55 @@ namespace Lab_2
             private set { _mileage = value; }
         }
 
+        // Constructors
+
+        Bus(long seed = 27032002)
+        {
+            _id = seed.GetHashCode();
+        }
+
+        static Bus()
+        {
+            m_availableBrands = new string[] { "МАЗ-103", "МАЗ-104", "МАЗ-105", "МАЗ-107", "МАЗ-152", "МАЗ-203", "МАЗ-206", "МАЗ-251", "МАЗ-256" };
+        }
+
+        public Bus() : this(DateTime.Now.Millisecond)
+        {
+            Brand = m_availableBrands[Application.rand.Next(m_availableBrands.Length)];
+            StartDate = new DateTime(year: Application.rand.Next(1997, DateTime.Now.Year - 1),
+                                     month: Application.rand.Next(1, 12),
+                                     day: Application.rand.Next(1, 31));
+            Mileage = (DateTime.Now.Hour - StartDate.Hour) * Application.rand.Next(50, m_maxSpeed);
+        }
+
+        public Bus(string busNumber = "1337 IT-4") : this()
+        {
+            BusNumber = busNumber;
+        }
+
+        public Bus(Driver driver, short wayNumber) : this()
+        {
+            Driver = driver;
+            WayNumber = wayNumber;
+        }
+
         // Methods
+
+        public int GetBusAge(out int currentYear)
+        {
+            currentYear = DateTime.Now.Year;
+            return currentYear - StartDate.Year;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (BusNumber == ((Bus)obj).BusNumber && Brand == ((Bus)obj).Brand);
+        }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode();
+        }
 
         public override string ToString()
         {
