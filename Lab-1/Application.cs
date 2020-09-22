df465dda56b1d3471ea0edd8f156e888ed1c2786
Dmitriy Khudnitskiy (VS) <dimon.h02@gmail.com>
2020-09-22 21:03:15 +0300

    Какой-то using...

--- Lab-1/Application.cs
+++ Lab-1/Application.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data.SqlTypes;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Schema;
