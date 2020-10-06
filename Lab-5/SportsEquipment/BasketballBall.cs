2423896c7118a9ec18ad6dff659be29d0c1be933
Dmitriy Khudnitskiy (VS) <dimon.h02@gmail.com>
2020-10-06 22:04:38 +0300

    added a base for 5th lab

---
+++ Lab-5/SportsEquipment/BasketballBall.cs
@@ -0,0 +1,13 @@
+﻿using Lab_5.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5.SportsEquipment
+{
+    public class BasketballBall : Equipment, IBall
+    {
+    }
+}
---
+++ Lab-5/Tennis.cs
@@ -0,0 +1,14 @@
+﻿using Lab_5.SportsEquipment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    public class Tennis
+    {
+        Equipment[] equipments;
+    }
+}
