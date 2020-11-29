0957f3cba52858c9e165bf0e5901ad47a8d52cad
Dmitriy Khudnitskiy (VS) <dimon.h02@gmail.com>
2020-11-29 18:14:21 +0300

    oh, yeah boys

--- Lab-12/Application.cs
+++ Lab-12/Application.cs
@@ -11,18 +11,19 @@ namespace Lab_12
 {
     class Application
     {
+        public static Regex busNumberPattern = new Regex(@"\d{4}\s\w{2}-[0-9]");
+        public static Random rand = new Random(DateTime.Now.Millisecond);
         public static void Main(string[] args)
         {
-
             try
             {
-                Reflector.InvokeMethods("Lab_12.Reflector");
-                Reflector.GetClassInformation("Lab_12.Reflector");
+                Reflector.InvokeMethods("Lab_12.Bus");
+                Reflector.GetClassInformation("Lab_12.Bus");
             }
             catch (Exception e)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"\n\t{e.Message}");
+                Console.WriteLine($"\n\t{e.Message}\n{e.StackTrace}");
                 Console.ResetColor();
             }
             Console.ReadKey();
@@ -39,19 +40,23 @@ namespace Lab_12
             return Type.GetType(objName, false, true) ?? throw new Exception("Class type with this name cannot be found.");
         }
 
-        private static void WriteToFile(FileStream fout, string str)
+        private static void WriteToFile(string fileName, string str, FileMode mode = FileMode.Create)
         {
+            FileStream fout = new FileStream(fileName, mode);
             byte[] data = Encoding.Default.GetBytes(str);
             fout.Write(data, 0, data.Length);
 #if DEBUG
             Console.WriteLine(str);
 #endif
+            fout.Close();
         }
 
-        private static string ReadFromFile(FileStream fin)
+        private static string ReadFromFile(string fileName)
         {
+            FileStream fin = new FileStream(fileName, FileMode.Open);
             byte[] data = new byte[fin.Length];
             fin.Read(data, 0, data.Length);
+            fin.Close();
             return Encoding.Default.GetString(data);
         }
 
@@ -66,14 +71,16 @@ namespace Lab_12
 
         public static void InvokeMethods(string objName)
         {
-            FileStream fin = new FileStream(objName + "_args.txt", FileMode.Open);
-            if (fin == null)
-                throw new Exception("No argument file found for the method.");
-
             Type t = GetObjectType(objName);
-            foreach (var x in from l in ReadFromFile(fin).Split('\n') select Regex.Split(l, @"\s*[->]\s*"))
+            var obj = Activator.CreateInstance(t);
+            foreach (var args in from l in ReadFromFile(objName + "_args.txt").Split('\n')
+                              select Regex.Split(l, @"\s*[->]\s*").Where(x => x != string.Empty).Select(x => Regex.Replace(x, @"[()\r]", "")))
             {
-                t.GetMethod(x.LastOrDefault()).Invoke(Activator.CreateInstance(t), x.FirstOrDefault().Split(','));
+                MethodInfo method = t.GetMethod(args.LastOrDefault());
+                object[] prms = (from p in args.FirstOrDefault().Split(',').Where(x => !x.Equals(string.Empty))
+                                from pT in method.GetParameters().Select(x => x.ParameterType)
+                                select Convert.ChangeType(p, pT)).ToArray();
+                Console.WriteLine($"{args.LastOrDefault()}() -> {method.Invoke(obj, prms) ?? "void"}");
             }
         }
 
@@ -81,8 +88,7 @@ namespace Lab_12
         {
             Type t = GetObjectType(objName);
 
-            FileStream fout = new FileStream(objName + ".txt", FileMode.OpenOrCreate);
-            WriteToFile(fout, string.Join("\n", TableFormat(new string[] {
+            WriteToFile(objName + ".txt", string.Join("\n", TableFormat(new string[] {
                 $"Namespace: {t.Namespace}",
                 $"Name: {t.Name}\n",
                 $"IsAbstract: {t.IsAbstract}",
@@ -92,12 +98,11 @@ namespace Lab_12
                 $"IsInterface: {t.IsInterface}",
                 $"IsSealed: {t.IsSealed}",
                 $"IsValueType: {t.IsValueType}",
-
             })));
-            WriteToFile(fout, "\n\n Fields:\n\n\t" + string.Join("\n\t", GetClassFields(t.FullName)));
-            WriteToFile(fout, "\n\n Properties:\n\n\t" + string.Join("\n\t", GetClassProperties(t.FullName)));
-            WriteToFile(fout, "\n\n Methods:\n\n\t" + string.Join("\n\t", GetClassMethods(t.FullName)));
-            WriteToFile(fout, "\n\n Interfaces:\n\n\t" + string.Join("\n\t", GetClassInterfaces(t.FullName)));
+            WriteToFile(objName + ".txt", "\n\n Fields:\n\n\t" + string.Join("\n\t", GetClassFields(t.FullName)), FileMode.Append);
+            WriteToFile(objName + ".txt", "\n\n Properties:\n\n\t" + string.Join("\n\t", GetClassProperties(t.FullName)), FileMode.Append);
+            WriteToFile(objName + ".txt", "\n\n Methods:\n\n\t" + string.Join("\n\t", GetClassMethods(t.FullName)), FileMode.Append);
+            WriteToFile(objName + ".txt", "\n\n Interfaces:\n\n\t" + string.Join("\n\t", GetClassInterfaces(t.FullName)), FileMode.Append);
         }
         public static string[] GetClassFields(string objName) => TableFormat((from f in GetObjectType(objName).GetFields()
                                                                               select f.ToString()).ToArray());
