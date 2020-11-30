3942acbd9c1de8b1e3bdd7625958c5b15ad2386d
Dmitriy Khudnitskiy (VS) <dimon.h02@gmail.com>
2020-11-30 17:44:04 +0300

    14th start :D

---
+++ Lab-14/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_14
+{
+    public class Application
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Lab-14/Serializator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Runtime.Serialization.Json;
+using System.Xml.Serialization;
+
+namespace Lab_14
+{
+    public class Serializator
+    {
+        public void SerializeToBinary(object obj)
+        {
+            using (FileStream fout = new FileStream(obj.GetType().FullName + ".bin", FileMode.Create))
+            {
+                BinaryFormatter binary = new BinaryFormatter();
+            }
+        }
+
+        public void SerializeToSOAP(object obj)
+        {
+            using (FileStream fout = new FileStream(obj.GetType().FullName + ".", FileMode.Create))
+            {
+                SoapFormatter soap = new SoapFormatter();
+            }
+        }
+
+        public void SerializeToJSON(object obj)
+        {
+            using (FileStream fout = new FileStream(obj.GetType().FullName + ".", FileMode.Create))
+            {
+                DataContractJsonSerializer json = new DataContractJsonSerializer();
+            }
+        }
+
+        public void SerializeToXML(object obj)
+        {
+            using (FileStream fout = new FileStream(obj.GetType().FullName + ".", FileMode.Create))
+            {
+                XmlSerializer xml = new XmlSerializer();
+            }
+    }
+}
